03fb485984428f5197774a37cd0aed3731007168
Vladislav-web <ykt.pc_master@mail.ru>
2022-02-05 02:53:06 +0300

    added first practice

---
+++ js/type-classific.js
@@ -0,0 +1,28 @@
+'use strict';
+
+let number = 4.6;
+
+console.log(-4/0);
+console.log('string' * 9);
+
+const person = `5`;
+
+const bool = false;
+
+// console.log(something);
+
+let und;
+console.log(und);
+
+const obj = {
+  name: "John",
+  age: 25,
+  isMarriied: false
+};
+
+// console.log(obj.name);
+console.log(obj["name"]);
+
+
+let arr = ['plumb.png', 'orange.jpg', 6, 'apple.bmp', {}, []];
+console.log(arr[1]);
